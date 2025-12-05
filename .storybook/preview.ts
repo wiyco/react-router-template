f0c56489f4bd0e0e4b6d9fdb1a30a01f6a126e58
wiyco <72733890+wiyco@users.noreply.github.com>
2025-12-06 07:07:24 +0900

    chore(storybook): bump storybook to v10

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,6 @@
 import "~/styles/tailwind.scss";
 
-import type { Preview } from "@storybook/react";
+import type { Preview } from "@storybook/react-vite";
 
 const preview: Preview = {
   parameters: {
